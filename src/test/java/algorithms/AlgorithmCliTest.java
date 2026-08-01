8166731a26c5fa2e65ca69c98633b5e2b720c764
Andrei Ovcharenko <a@krot.name>
2026-08-01 04:50:23 +0300

    Handle malformed calculator expressions

--- src/test/java/algorithms/AlgorithmCliTest.java
+++ src/test/java/algorithms/AlgorithmCliTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @Tag("integration")
 class AlgorithmCliTest {
@@ -48,6 +49,12 @@ class AlgorithmCliTest {
             Files.deleteIfExists(input);
             Files.deleteIfExists(output);
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"+", "", "1 0 /", "abc", "1 2"})
+    void calculatorRejectsMalformedExpressionsWithoutThrowing(String input) throws Exception {
+        assertEquals("", invokeWithStdio("algorithms.sprint2.Calculator", "run", input));
     }
 
     private static Stream<Arguments> stdioCases() {
